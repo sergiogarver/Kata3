8cfa65f3a52fd91889398d992b3c85505c538f74
Sergio Garcia <sergiogarciavera1997@gmail.com>
2017-10-23 21:46:36 +0100

    Versión 2

---
+++ src/kata3/Histogram.java
@@ -0,0 +1,21 @@
+package kata3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Histogram<T> {
+    private final Map<T,Integer> map = new HashMap<>();
+    
+    public int get(T key){
+        return(map.get(key));
+    }
+    
+    public Set<T> keySet(){
+        return map.keySet();
+    }
+    
+    public void increment(T key, int value){
+        map.put(key, map.containsKey(key) ? map.get(key) +value : value);
+    }
+}
