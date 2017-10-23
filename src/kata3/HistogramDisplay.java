91a166aa1eb15f20fa80338fa7fcd5ea2845a72a
Sergio Garcia <sergiogarciavera1997@gmail.com>
2017-10-23 19:07:38 +0100

    Versión 2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -8,11 +8,14 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
+import java.lang.String;
 
 public class HistogramDisplay extends ApplicationFrame {
-
-    public HistogramDisplay() {
+    
+    private final Histogram<String> histogram;
+    public HistogramDisplay(Histogram<String> histogram){
         super("HISTOGRAMA");
+        this.histogram = histogram;
         setContentPane(createPanel());
         pack();
     }
@@ -36,10 +39,9 @@ public class HistogramDisplay extends ApplicationFrame {
     
     private DefaultCategoryDataset createDataset(){
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        dataSet.addValue(5," ", "gmail.com");
-        dataSet.addValue(10," ", "ulpgc.es");
-        dataSet.addValue(7," ", "ull.es");
-        dataSet.addValue(2," ", "hotmail.com");
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
         return dataSet;
         
     }
