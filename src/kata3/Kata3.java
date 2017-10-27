0ffb06370e7f658e2c2eee61ef7d8de7459a5d11
Sergio Garcia <sergiogarciavera1997@gmail.com>
2017-10-27 13:50:31 +0100

    Versión 2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,8 +3,12 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay();
-        histo.execute();
+        Histogram<String> histogram = new Histogram<String>();
+        histogram.increment("gmail.com",1);
+        histogram.increment("ulpgc.es",3);
+        histogram.increment("hotmail.com",1);
+        histogram.increment("ull.es",2);
+        new HistogramDisplay(histogram).execute();
     }
     
 }
